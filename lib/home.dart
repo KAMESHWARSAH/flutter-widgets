11115b2f39f9eb808cd4f99762da451eaa1e86d1
kameshwar sah <56642875+kameshwar-web@users.noreply.github.com>
2022-04-27 18:25:20 +0530

    cheld add

--- lib/home.dart
+++ lib/home.dart
@@ -8,5 +8,22 @@ class MyHomePage extends StatelessWidget {
         title: Text("Hello Flutter "),
       ),
     );
+    Container(
+      height: 500,
+      width: 400,
+      child: Center(
+        child: Container(
+          height: 120.0,
+          width: 120.0,
+          color: Colors.blue[50],
+          child: const Align(
+            alignment: Alignment.topRight,
+            child: FlutterLogo(
+              size: 60,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
